1a3ca0b29bcab9a27ca1a6a414b650a3711c6620
grypton2 <mdarif.updated@gmail.com>
2025-07-19 18:47:13 +0000

    added script for nginx changes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 New repo to test jenkins with AWS EC2
 Hello from EC2 local
 Adding more details for setting up creds
+Adding even more details here
--- nginx_file.sh
+++ nginx_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install nginx -y
-echo "Running the code via jenkins" | sudo tee /var/www/html/index.html
+echo "Running the code via jenkins and now this is update more" | sudo tee /var/www/html/index.html
