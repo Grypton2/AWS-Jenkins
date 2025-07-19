111eceb20361ad30e278c9be1b8323706ae98c8a
grypton2 <mdarif.updated@gmail.com>
2025-07-19 18:04:06 +0000

    added script for nginx update

--- nginx_file.sh
+++ nginx_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update 
 apt-get install nginx -y
-echo "Running the code via jenkins" -> /var/www/html/index.html
+echo "Running the code via jenkins" > /var/www/html/index.html
